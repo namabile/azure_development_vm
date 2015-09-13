2f069e6cd66ed8db975741320ab5d9c561cd8eae
Nick Amabile <amabile.n@gmail.com>
2015-09-13 12:04:32 -0400

    update roles

--- roles/eventstore.rb
+++ roles/eventstore.rb
@@ -3,8 +3,8 @@ description "for systems that will host an eventstore server"
 
 run_list "role[base], recipe[eventstore]"
 
-override_attributes [
+override_attributes(
   "eventstore" => {
     "source_url" => "http://download.geteventstore.com/binaries/EventStore-OSS-Ubuntu-v3.2.0.tar.gz"
   }
-]
+)
--- roles/hadoop_nn.rb
+++ roles/hadoop_nn.rb
@@ -4,5 +4,12 @@ description "for systems acting as the hadoop namenode"
 run_list [
   "role[base]",
   "recipe[hadoop::hadoop_hdfs_namenode]",
-  "recipe[hadoop_wrapper::hadoop_hdfs_namenode_init]"
+  "recipe[hadoop_wrapper::hadoop_nn_init]",
+  "recipe[hadoop_wrapper::hadoop_nn_start]"
 ]
+
+override_attributes(
+  "hadoop" => {
+    "distribution" => "cdh"
+  }
+)
